7593437f49bff81be735013d383ee122c6d8c422
Konrad Bartlett <konradbartlett@trentu.ca>
2018-03-29 16:03:49 -0400

    garbage

--- CalendarPane/src/Main.java
+++ CalendarPane/src/Main.java
@@ -1,15 +1,33 @@
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import java.util.Locale;
 
-import java.time.YearMonth;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Full Calendar Example");
-        primaryStage.setScene(new Scene(new FullCalendarView(YearMonth.now()).getView()));
+    public void start(Stage primaryStage) {
+
+        CalendarView calendarView = new CalendarView() ;
+
+        Button next = new Button(">");
+        next.setOnAction(e -> calendarView.nextMonth());
+
+        Button previous = new Button("<");
+        previous.setOnAction(e -> calendarView.previousMonth());
+
+
+
+        BorderPane root = new BorderPane(calendarView.getView(), null, next, null, previous);
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
